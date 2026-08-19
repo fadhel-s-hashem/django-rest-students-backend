1ca059ca087b42154aa8a0c936212b7072128dc9
Fadhel <fadhel.s.hashem@gmail.com>
2026-08-19 16:33:29 +0300

    BAS_DIR & SECRET_KEY

--- student_api/settings.py
+++ student_api/settings.py
@@ -18,13 +18,17 @@ from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / ".env")
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%31&2!^6m6wqh4tm@w^_u7^4#b5vwwx4v2063gmhzyf&%^kn@0'
+SECRET_KEY = os.getenv(
+    "SECRET_KEY",
+    "django-insecure-classroom-only-not-for-production-12345",
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
