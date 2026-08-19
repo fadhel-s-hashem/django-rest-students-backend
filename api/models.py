4366d1f4b454a90936464c2ff2e35d94cf09d9fc
Fadhel <fadhel.s.hashem@gmail.com>
2026-08-19 18:36:56 +0300

    finish student model

--- api/models.py
+++ api/models.py
@@ -1,13 +1,22 @@
 from django.db import models
 
-
+#  almos how every model well look like 
 class Student(models.Model):
     # Add the three fields.
+    GENDER_CHOICES = [
+        ('Male'),
+        ('Female'),
+
+    ]
+
+    name = models.CharField(max_length=100)
+    favorite_food = models.CharField(max_length=100)
+    favorite_emoji = models.CharField(max_length=20)
 
     class Meta:
         # Add alphabetical ordering.
-        pass
+        ordering = ['name'] # alphabetical ordering by name
 
     def __str__(self):
 
-        pass
+        return self.name
