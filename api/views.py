5605ebd52a4e7d1b44f8acb851922b3a33ec93a1
Fadhel <fadhel.s.hashem@gmail.com>
2026-08-20 11:28:24 +0300

    basic imports in viewrs (controller)

--- api/views.py
+++ api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+#  necesary tamplet for the viewrs (controller)
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+# from rest_framework_simplejwt.tokens import RefreshToken
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+# import the models and serializer
+from .models import Student
+from .serializers import StudentSerializer
